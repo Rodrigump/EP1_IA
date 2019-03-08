f7c447d46037513471dd6a484f4c2306f693bb24
Rodrigump <rodrigo.rossi.santos@usp.br>
2019-03-08 13:20:33 -0300

    Update EP1_IA.py
    Inserção da função getN()

--- EP1_IA.py
+++ EP1_IA.py
@@ -1,15 +1,18 @@
 import math
 
-#função que calcula o valor Q
+#função que calcula o valor de q, dado max, min e número de bits
 def getQ (max, min, n):
-    return (max-min)/(math.pow(2,n)-1)
+    return (max-min)/((2**n)-1)
 
+#função que calcula o número de bits necessários, dado max, min e q
+def getN (max, min, q):
+	return math.floor(math.log(((max-min)/q)+1) / math.log(2))
 
 #função que converte uma cadeia de binários em um número real
 def bin_dec(bin):
 	res = 0
 	for i in range(len(bin)-1,-1,-1):
 		aux = float(bin[i])
-		res = res + ((aux)*math.pow(2,(-i + len(bin) - 1)))
+		res = res + ((aux)*(2**((-i + len(bin) - 1))))
 		i=i-1
 	return res
