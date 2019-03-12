e6a18f8fc0db3805400bb9a587d69a212b605447
victor-reis <victor.moreira@netshoes.com>
2019-03-12 00:18:09 -0300

    Cross_over and changed 'genoma' to 'genome'

--- EP1_IA.py
+++ EP1_IA.py
@@ -17,10 +17,24 @@ def bin_dec(bin):
 		res = res + ((aux)*(2**((-i + len(bin) - 1))))
 		i=i-1
 	return res
-
 #Função para gerar o genoma (vetor binário) aleatório
-def random_genoma(size):
+def random_genome(size):
     return [random.randrange(0, 2) for _ in range(0, size)]
 
-#Convertendo o genoma em valor real
-print(bin_dec(random_genoma(5)))
+#Assumindo que genoma 1 e 2 são do mesmo tamanho
+def cross_over(first_genome,second_genome):
+    cross_over_point = random.randint(0,(len(first_genome)))
+    for pos in range(0,cross_over_point):
+        temp = first_genome[pos]
+        first_genome[pos] = second_genome[pos]
+        second_genome[pos] = temp
+
+#sample code
+first_genome = [0,0,0,0,0]
+second_genome = [1,1,1,1,1]
+print("Vetor A %s" % first_genome)
+print("Vetor B %s" % second_genome)
+
+cross_over(first_genome,second_genome)
+print("Vetor A' %s" % first_genome)
+print("Vetor B' %s" % second_genome)
