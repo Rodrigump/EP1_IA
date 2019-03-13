13db457841708596c3ddc11d579b1f35a1053807
Rodrigump <rodrigo.rossi.santos@usp.br>
2019-03-13 14:30:22 -0300

    Update EP1_IA.py
    Inserção de uma função de fitness (temporariamente hipotética), do algoritmo da roleta e das variáveis fenótipo e fitness na classe Individuo.

--- EP1_IA.py
+++ EP1_IA.py
@@ -1,29 +1,13 @@
 import math
 import random
 
-class Individuo:
-    
-    def __init__ (self, no_genes): #Construtor da classe
-        self.no_genes = no_genes #tamanho da cadeia
-        self.genotipo = random_genome(no_genes) #cadeia de bits
-            
-    def imprime (self): #imprime a cadeia de bits
-        for i in range(self.no_genes):
-            print(self.genotipo[i],end="",flush=True)
-        print()
-            
-class Populacao:
-    
-    def __init__ (self, no_genes, no_individuos): #Construtor da classe
-        self.no_genes = no_genes
-        self.no_individuos = no_individuos #numero de individuos da populacao
-        self.individuos = [] #arranjo de individuos
-        for i in range(no_individuos):
-            self.individuos.append(Individuo(no_genes))
-    
-    def imprime(self): #imprime o genotipo de todos os individuos da populacao
-        for i in range(self.no_individuos):
-            self.individuos[i].imprime()
+#valores hipoteticos
+min = 1
+max = 100
+q = 0.5
+
+def f(x): #funcao hipotetica de fitness
+	return math.sin(x) + math.sqrt(x)
 
 #função que calcula o valor de q, dado max, min e número de bits
 def getQ (max, min, n):
@@ -52,6 +36,44 @@ def cross_over(first_genome,second_genome):
         temp = first_genome[pos]
         first_genome[pos] = second_genome[pos]
         second_genome[pos] = temp
+	
+class Individuo:
+    
+    def __init__ (self, no_genes): #Construtor da classe
+        self.no_genes = no_genes #tamanho da cadeia
+        self.genotipo = random_genome(no_genes) #cadeia de bits
+	self.fenotipo = min + (q * bin_dec(self.genotipo)) #converte a cadeia para um real com ponto flutuante
+        self.fitness = f(self.fenotipo) #aplica o valor do fenotipo a funcao de fitness
+            
+    def imprime (self): #imprime a cadeia de bits
+        for i in range(self.no_genes):
+            print(self.genotipo[i],end="",flush=True)
+        print()
+            
+class Populacao:
+    
+    def __init__ (self, no_genes, no_individuos): #Construtor da classe
+        self.no_genes = no_genes
+        self.no_individuos = no_individuos #numero de individuos da populacao
+        self.individuos = [] #arranjo de individuos
+        for i in range(no_individuos):
+            self.individuos.append(Individuo(no_genes))
+    
+    def imprime(self): #imprime o genotipo de todos os individuos da populacao
+        for i in range(self.no_individuos):
+            self.individuos[i].imprime()
+	
+    def roleta (self): #retorna o indice do individuo escolhido na roleta
+        somaPesos = 0
+        for i in range (self.no_individuos):
+            somaPesos = somaPesos + self.individuos[i].fitness
+        
+        valor = somaPesos * random.uniform(0,1)
+        for i in range(self.no_individuos):
+            valor = valor - self.individuos[i].fitness
+            if(valor < 0):
+                return i
+        return self.no_individuos - 1	
 
 #sample code
 first_genome = [0,0,0,0,0]
